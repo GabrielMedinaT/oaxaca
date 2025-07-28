6218838a9ec27e37bcf03528aa8c08c2d9241089
Gabriel <gabrielmedinatorres@hotmail.com>
2025-07-28 18:53:29 +0100

    version 1

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="coming-soon-container">
+      <div className="logo"></div>
+      <div className="overlay">
+        <h1 className="title">Finca Oaxaca</h1>
+        <h4 className="subtitle">Próximamente</h4>
+        <h5></h5>
+        <p className="message">
+           Un lugar donde el tiempo se detiene y la naturaleza te envuelve, invitándote a desconectar del mundo y reencontrarte contigo mismo. 
+        </p>
+        <p className="message2" >Aquí, perderse es volver a encontrarse: un refugio donde la calma abraza al alma y cada rincón invita a descubrir nuevas experiencias que despiertan los sentidos</p>
+        <p className="message3">Déjate guiar por la serenidad de este espacio único, donde el cuerpo y el espíritu encuentran descanso, y cada momento se transforma en una oportunidad para vivir algo especial.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
